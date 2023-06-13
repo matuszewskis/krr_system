8a6308d3237701adfe835e8d38f707102ea8b4b3
Piotr Sieńko <43791179+piosienk@users.noreply.github.com>
2023-06-13 19:25:08 +0200

    Update test_PS.py
    PS tests update

--- test/test_PS.py
+++ test/test_PS.py
@@ -1,35 +1,39 @@
 import unittest
 
 from sympy import Symbol
-
 from krr_system import TimeDomainDescription, Scenario
 
 
 class MyTestCase(unittest.TestCase):
-    def test_Q1_PS(self):
+    def test_Q1_Q2_PS(self):
         """This scenario is consistent"""
         f = Symbol("flying")
         r = Symbol('refuelled')
-        c = Symbol('crash')
 
         d = TimeDomainDescription()
-        d.initially(flying=False, refuelled=False, crash=False)
-        d.causes('refuel', r, conditions= ~f & ~c)
-        d.causes("takeoff", f, conditions= r & ~c)
+        d.initially(flying=False, refuelled=True, crash=False)
+        d.causes('refuel', r, conditions= ~f )
+        d.causes("takeoff", f, conditions= r )
         d.causes("land", ~f & ~r, conditions=f)
-        d.releases("takeoff", c & ~f)
         d.duration("refuel", 1)
         d.duration("takeoff", 2)
         d.duration("land", 1)
 
         d.terminate_time(8)
 
-        OBS = ((~f & ~r & ~c, 0),)
-        ACS = (("refuel", 1),("takeoff", 2))
+        OBS = [(~f & r, 0)]
+        ACS = [("refuel", 1), ("takeoff", 2)]
         s = Scenario(domain=d, observations=OBS, action_occurrences=ACS)
 
+        # Q1
         print("S is consistent? - ", s.is_consistent())
         self.assertEqual(s.is_consistent(verbose=True), True)
+        # Q2
+        print("does in flight hold at timepoint 4 - ", s.check_if_condition_hold(f, 3))
+        self.assertEqual(s.check_if_condition_hold(f, 3), None)
+        # Q2
+        print("does in flight hold at timepoint 4 - ", s.check_if_condition_hold(f, 4))
+        self.assertEqual(s.check_if_condition_hold(f, 4), True)
 
 
 if __name__ == '__main__':
